cb6b7d21eb8c650c496a1afe2df24abce7b4171f
okayung <okay5770@naver.com>
2024-05-31 08:42:58 +0900

    수정

--- box-layout/position/script.js
+++ box-layout/position/script.js
@@ -4,10 +4,9 @@ let popupOpenBtn = document.querySelector('.popup-open-btn');
 let popupCloseBtn = document.querySelector('.popup-close-btn');
 
 popupOpenBtn.addEventListener('click', function(){
-  popup.classList.add(is-active);
+  popup.classList.add('is-active');
 })
 
 popupCloseBtn.addEventListener('click', function(){
-  popup.classList.remove(is-active);
+  popup.classList.remove('is-active');
 })
-console.log(popup, popupCloseBtn)
