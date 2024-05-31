a1878d731a920d112e52cb63772f0dd861acc400
okayung <okay5770@naver.com>
2024-05-31 10:52:12 +0900

    매개변수

--- box-layout/position/실습0530/script.js
+++ box-layout/position/실습0530/script.js
@@ -15,7 +15,6 @@ popupCloseBtn.addEventListener('click', function(){
 let sidebar = document.querySelector('.sidebar');
 let sidebarOpenBtn = document.querySelector('.sidebar-open-btn');
 let sidebarCloseBtn = document.querySelector('.sidebar-close-btn');
-console.log(sidebar, sidebarOpenBtn, sidebarCloseBtn)
 // 1.sidebaropenbar 을 클릭했을때
 //  sidebar 에 is-active 클래스 추가
 
@@ -25,4 +24,11 @@ sidebarOpenBtn.addEventListener('click', function(){
 sidebarCloseBtn.addEventListener('click', function(){
   sidebar.classList.remove('is-active')
 })
-
+// 함수 소괄호 안 :매개변수
+function plus (unm1, unm2) {
+  console.log(unm1 + unm2)
+  console.log(unm1, unm2);
+  // 실행 할 코드
+}
+// 함수는 호출했을 때 실행됨.
+plus(100, 22);
