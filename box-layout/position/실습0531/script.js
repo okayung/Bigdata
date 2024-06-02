f51b658e8682334b72ad801c5b8171fe463d37a7
okayung <okay5770@naver.com>
2024-06-02 12:21:15 +0900

    수정

---
+++ box-layout/position/실습0531/script.js
@@ -0,0 +1,4 @@
+let sidebar = document.querySelector('.sidebar')
+let sidebarOpenBtn = document.querySelector('.sidebar-open-btn')
+let sidebarCloseBtn = document.querySelector('.sidebar-close-btn')
+
